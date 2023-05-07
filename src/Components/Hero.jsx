d60b9e44ecba8c763196baa294be732d5ebc2803
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-05-07 23:31:16 +0100

    Hero section first modifications
    ce sont les premieres modification de la partie dacceuil  et du fichier package . json

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -20,11 +20,11 @@ const Hero = ({ index }) => {
       <div className='header-content'>
 
         <div className=' part2 '>
-
-          <h1 className='headertext'> Un over our servicis and greate proposal for clients </h1>
-
-          <div className='buttons'>
-            <button href='' className=' ' >  contact us </button>
+          <h5 className='text-yellow pb-4' >Custom services</h5>
+          <h1 className='headertext font-black text-6xl pb-4'> Run over our servicis and greate proposal for clients </h1>
+          <h5 className='text-white'>Ask what you want and we will deliver it intiome</h5>
+          <div className='buttons absolute top-96'>
+            <button href='' className=' ' >  Contact us </button>
             <button href='' className='cb'  >Donate</button>
           </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,41 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'yellow': '#eab308',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontWeight: {
+
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+
+      'extra-bold': '800',
+      black: '900',
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '40rem',
+    },
   },
   plugins: [],
 }
