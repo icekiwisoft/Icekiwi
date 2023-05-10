b05e1d0e59c7128834a6e19a9a9c3849990e53a9
Martialmonjoh <115487309+Martialmonjoh@users.noreply.github.com>
2023-05-10 16:06:20 +0200

    teamfooter

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,8 +1,13 @@
 import './team.scss'
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
 export default function Team() {
     return (
         <div className="about">
+            <Navbar/>
             ici sera la team faite parler votre imagination
+            <Footer />
         </div>
+        
     )
 }
