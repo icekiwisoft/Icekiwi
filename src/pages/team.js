d67b65e2dd253befaef792a25aaa5622831a7b6b
Martialmonjoh <115487309+Martialmonjoh@users.noreply.github.com>
2023-05-10 18:35:35 +0200

    teampage

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,13 +1,176 @@
 import './team.scss'
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook,faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 export default function Team() {
     return (
         <div className="about">
-            <Navbar/>
-            ici sera la team faite parler votre imagination
-            <Footer />
+            <Navbar />
+            <div class="col">
+                <h2 class="texte">Our Expert Team</h2>
+            </div>
+
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="team-box">
+                            <div class="team-media">
+                                <img src={image1} alt="" />
+                            </div>
+                            <div class="team-info">
+                                <h3>Johnathan Doe</h3>
+                                <p>Cardiologist</p>
+                                
+                                <ul class="team-social">
+							        <li> <a href=""><FontAwesomeIcon icon={faInstagram} /></a>  </li>
+							        <li> <a href=""> <FontAwesomeIcon icon={faFacebook} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faTwitter} /> </a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faGithub} /></a>  </li>
+							        <li> <a href=""><FontAwesomeIcon icon={faYoutube} /></a>  </li>
+							    </ul> 
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+            <Footer/>
         </div>
-        
+
+
     )
 }
