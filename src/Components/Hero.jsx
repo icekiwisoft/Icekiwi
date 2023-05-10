beed1e8ce08eceb64af6363e8fff914664629d80
ngdream <96875506+ngdream@users.noreply.github.com>
2023-05-10 22:45:37 +0100

    somes changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,21 +8,25 @@ import Navbar from './Navbar';
 const Hero = () => {
   return (
 
-    <header className='h-screen md:px-body md:pt-8 '>
-      <video src={vid} autoPlay muted loop />
+    <header className='min-h-screen md:px-body md:pt-8 '>
+
 
       <Navbar />
       <div className='header-content'>
 
         <div className=' part2 '>
-          <h5 className='text-yellow pb-4' >Custom services</h5>
+          <h5 className=' pb-4 text-white' >Custom services</h5>
           <h1 className='headertext font-black text-6xl pb-4'> Run over our servicis and greate proposal for clients </h1>
           <h5 className='text-white'>Ask what you want and we will deliver it in time</h5>
-          <div className='buttons absolute top-96'>
+          <div className='buttons '>
             <button href='' className=' ' >  Contact us </button>
             <button href='' className='cb'  >Donate</button>
           </div>
 
+        </div>
+
+        <div className='video-container'>
+          <video src={vid} autoPlay muted loop />
         </div>
 
       </div>
