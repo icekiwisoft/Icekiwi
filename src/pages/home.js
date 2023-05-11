7bf6dbd02cf9326957fcdc0f264eb4b672c35900
ngdream <96875506+ngdream@users.noreply.github.com>
2023-05-11 13:06:50 +0100

    update and add some picture

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <>
             <Hero />
             <About />
-            <section className="book px-5" style={{ backgroundImage: 'url(book.jpg)' }} >
+            <section className="book px-5" style={{ backgroundImage: 'url(book5.jpg)' }} >
 
                 <div className='content'>
 
@@ -15,7 +15,7 @@ export default function Home() {
                         <span className="text-5xl font-bold text-blue">
                             literary service</span>
 
-                        <p className="text-gray text-xl">
+                        <p className="text-black text-xl">
                             we offer content editing and translation services for translation services, you will send the text to our mailbox after we will send you an invoice and the translation time after payment and we will send you the translated document in time, 0.02$ min per word
                         </p>
                     </div>
