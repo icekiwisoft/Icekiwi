733a51fea3d20515025456d7ef77bd50871d93a3
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-07-05 20:08:59 +0100

    .

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,38 +1,37 @@
 import React from 'react'
 import jobit from '../assets/jobit.png'
-import { styles } from "../styles.js";
-import { motion } from "framer-motion";
-import { fadeIn, textVariant } from "../utils/motion.js";
-import { FaInstagram, FaFacebook, FaTwitter, FaGithub, FaYoutube } from 'react-icons/fa'
+import './Component.css'
 
 const About = () => {
   return (
 
     <section className="about p-[1.5rem] sm:p-[4rem] md:flex m-auto ">
-      <div className=' md:mr-9 ' >
+      <div className=' md:mr-16 ' >
         <div >
-          <p className='sm:text-[18px] text-[14px] text-[#27589e] font-semibold uppercase tracking-wider'>Our Works</p>
-          <h2 className='text-[#000000] font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>Overviev.</h2>
+          <p className='sm:text-[18px] text-[14px] text-[#27589e] font-semibold uppercase tracking-wider'>About Us</p>
+          <h2 className='text-[#000000] font-black md:text-[40px] sm:text-[50px] xs:text-[40px] text-[30px]'>We follow you.</h2>
         </div>
 
         <div className='w-full flex'>
-          <p className='mt-3 text-part text-[#0e0e29] font-light max-w-[40rem] leading-[30px]'>
+          <p className='mt-3 text-part text-right text-[#0e0e29] font-light max-w-[30rem] leading-[30px]'>
             We are skilled software developers with experience in
             programing languages like Python, Java, C++, C#
             JavaScript and expertise in frameworks like React, Node.js,
             Django.... We deliver quick sevices and collaborate closely with
             clients to create efficient, scalable, and user-friendly
-            solutions that solve real-world problems. Let's work
-            together to bring your ideas to life!
+            solutions that solve real-world problems.
           </p>
         </div>
+        <button className='button '>
+          viewthe team
+        </button>
       </div>
 
 
 
       <div className='mt-20 flex flex-wrap gap-7' >
 
-        <div className='bg-tertiary p-3 rounded-2xl sm:w-[500px] w-full'>
+        <div className='bg-tertiary p-3 rounded-2xl sm:w-[600px] w-full'>
           <div className='relative w-full h-[200px]'>
             <img
               src={jobit}
